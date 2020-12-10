b11d7d7adecdbfc8ced035362579eb2bb2f4f011
Киселев Иван <47575805+Kekcek@users.noreply.github.com>
2020-12-10 12:04:47 +0300

    Update observer.cpp

--- observer.cpp
+++ observer.cpp
@@ -1,1 +1,36 @@
+#include "Observer.h"
+
+Observer::Observer(Ippo* mod, string n)
+{
+	model = mod;
+	o_name = n;
+	model->attach(this);
+}
+
+Observer::Observer()
+{
+	model = NULL;
+}
+
+void Observer::update()//приходит сюда после заврешения забега
+{
+	bool win = getSubject()->getwinner();
+	cout << o_name;
+	if (win)
+	{
+		cout << " выиграл " << '\n';
+	}
+	else
+		cout << " проиграл " << '\n';
+}
+
+Ippo* Observer::getSubject()
+{
+	return model;
+}
+
+void Ippo::notify()
+{
+	views->update();
+}
 
