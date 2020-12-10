71f8ce81efccc87ae7a3fd3c6cfc1ed5d4bddc30
Киселев Иван <47575805+Kekcek@users.noreply.github.com>
2020-12-10 12:02:43 +0300

    Update Observer.h

--- Observer.h
+++ Observer.h
@@ -1,1 +1,16 @@
+#pragma once
+#include "Ippo.h"
+
+using namespace std;
+class Observer //смотрящий класс, типа пользователь
+{
+	Ippo* model;
+	string o_name;
+public:
+	Observer(Ippo* mod, string n);
+	Observer();
+	void update();
+protected:
+	Ippo* getSubject();
+};
 
