a89f875f912e69d7fc30cc8288338bf8dface52b
Киселев Иван <47575805+Kekcek@users.noreply.github.com>
2020-12-10 12:03:06 +0300

    Update ippo.h

--- ippo.h
+++ ippo.h
@@ -1,1 +1,34 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+class Ippo
+{
+protected:
+	class Observer* views;
+	string por;
+	string name;
+	int year;
+	int win;
+	int speed;
+	bool winner;
+public:
+	Ippo() {};
+	Ippo(string por, string name, int year);
+	void setspeed(int a);
+	int getspeed();
+	void setwin();
+	int getwin();
+	void setwinner(bool flag);
+	bool getwinner();
+	string getData(int type);
+	void attach(Observer* obs);
+	string myprint();
+	void notify();
+	~Ippo() {};
+};
 
